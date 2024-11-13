e3a5a3cb5368ef7beabd61c9b9e21cb38839ae25
Riley Farro <rileyfarro@gmail.com>
2024-11-13 15:46:17 +0800

    Add documentation to Alarm fields.

--- app/src/main/java/com/rpfcoding/snoozeloo/feature_alarm/domain/Alarm.kt
+++ app/src/main/java/com/rpfcoding/snoozeloo/feature_alarm/domain/Alarm.kt
@@ -2,6 +2,13 @@ package com.rpfcoding.snoozeloo.feature_alarm.domain
 
 import java.util.UUID
 
+/**
+ * @param hour Value can be from 0-23
+ * @param minute Value can be from 0-59
+ * @param enabled If true, it will be scheduled in AlarmManager
+ * @param repeatDays 0 is Monday, 1 is Tuesday, so on...
+ * @param volume Can be 0-100
+ */
 data class Alarm(
     val id: String = UUID.randomUUID().toString(),
     val name: String,
@@ -15,6 +22,9 @@ data class Alarm(
 ) {
     val isMorning: Boolean get() = hour in 0..11
 
+    /**
+     * Get the hour in 12-hour format. 13 = 1 PM
+     */
     val hourTwelve: Int get() = if(hour > 12) {
         hour - 12
     } else {
